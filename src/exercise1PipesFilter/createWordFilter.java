1385d1fa61047f468bb42e4b7d236a6d377b07cf
Prokyonn <dma2695@students.fhv.at>
2017-10-23 09:13:55 +0200

    initial commit

---
+++ src/exercise1PipesFilter/createWordFilter.java
@@ -0,0 +1,11 @@
+package exercise1PipesFilter;
+
+import pmp.filter.AbstractFilter;
+import pmp.interfaces.Writeable;
+
+import java.io.StreamCorruptedException;
+import java.security.InvalidParameterException;
+
+public class createWordFilter extends AbstractFilter<String, String[]>{
+
+}
